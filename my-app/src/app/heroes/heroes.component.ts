4eac0ad01c94b3d8b1c4565047d05ea2abc5311e
Wagner Pereira da Silva <wagnerpdasilva@hotmail.com>
2018-09-06 23:58:51 -0300

    estudos de angular primeiro comite

---
+++ my-app/src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './Hero';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Wagner'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  
+}
---
+++ my-app/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,7 @@
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Wagner' },
+    { id: 2, name: 'Mariana' },
+    { id: 3, name: 'Murilo' }
+  ];
